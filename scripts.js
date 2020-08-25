ab65f97e8f1641101451d987842324becbfa64d1
Yuri Alves <yuripiresalves@gmail.com>
2020-08-24 21:17:51 -0300

    add scripts

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for(let card of cards) {
+    card.addEventListener('click', () => {
+        modalOverlay.classList.add('active')
+    })
+}
+
+document.querySelector('.modal-close').addEventListener('click', () => {modalOverlay.classList.remove('active')})
