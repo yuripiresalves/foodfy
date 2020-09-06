e690afe0555a033c3c2dac6eebd9cfa2056f20c1
Yuri Alves <yuripiresalves@gmail.com>
2020-09-05 23:21:29 -0300

    create server

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-const oii;
+const express = require('express')
+const server = express()
+
+server.set('view engine', 'html')
+
+server.get('/', (req, res) => {
+    return res.render(index)
+})
+
+server.listen(5000, () => {
+    console.log("pai ta on")
+})
