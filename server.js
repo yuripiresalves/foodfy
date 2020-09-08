ea743f23ceca52fcafdaf6423ce718c6752b80a5
Yuri Alves <yuripiresalves@gmail.com>
2020-09-07 22:02:05 -0300

    add recipes data

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
+const recipes = require('./data')
 
 const server = express()
 
 server.use(express.static('public'))
 
-server.set('view engine', 'html')
+server.set('view engine', 'njk')
 
 nunjucks.configure('views', {
     express: server
 })
 
 server.get('/', (req, res) => {
-    return res.render('index')
+    return res.render('index', { recipes })
 })
 
 server.get('/about', (req, res) => {
@@ -20,7 +21,7 @@ server.get('/about', (req, res) => {
 })
 
 server.get('/recipes', (req, res) => {
-    return res.render('recipes')
+    return res.render('recipes', { recipes })
 })
 
 server.listen(5000, () => {
