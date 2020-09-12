d331a570e155d3287bd307b2a73a0d03189cc398
Yuri Alves <yuripiresalves@gmail.com>
2020-09-11 21:17:01 -0300

    update

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ server.get('/recipes', (req, res) => {
     return res.render('recipes', { recipes })
 })
 
+server.get("/recipes/:index", function (req, res) {
+    const recipes = [] // Array de receitas carregadas do data.js
+    const recipeIndex = req.params.index
+  
+    return res.render('recipe', { recipes })
+    console.log(recipes[recipeIndex]);
+  })
+
 server.listen(5000, () => {
     console.log("pai ta on")
 })
