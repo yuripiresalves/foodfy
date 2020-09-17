a97526091a1528914651aca32ca16ceb67a72b98
Yuri Alves <yuripiresalves@gmail.com>
2020-09-16 21:58:23 -0300

    update

--- public/scripts-recipe.js
+++ public/scripts-recipe.js
@@ -1,1 +1,9 @@
-const hide = document.querySelectorAll('span')
+const botoes = document.querySelectorAll('h1 span')
+
+for (const botao of botoes) {
+    botao.addEventListener('click', () => {
+        console.log('cliquei')
+        botao.classList.add('invisible')
+    })
+}
+
