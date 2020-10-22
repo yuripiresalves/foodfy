1afa23285c68f9c0f0969abaf791d93bcf9b7e96
Yuri Alves <yuripiresalves@gmail.com>
2020-10-22 16:08:31 -0300

    add show and edit routes

--- recipes.js
+++ recipes.js
@@ -5,3 +5,11 @@ exports.index = (req, res) => {
 exports.create = (req, res) => {
   return res.render('admin/recipes/create')
 }
+
+exports.show = (req, res) => {
+  return res.render('admin/recipes/show')
+}
+
+exports.edit = (req, res) => {
+  return res.render('admin/recipes/edit')
+}
