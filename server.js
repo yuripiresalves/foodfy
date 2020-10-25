62e0c8003a43bb1c24e1f113c95ed4aefc25f181
Yuri Alves <yuripiresalves@gmail.com>
2020-10-24 22:43:36 -0300

    add methodOverride

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
-const recipes = require('./data')
+const recipes = require('./data.json')
 const routes = require('./routes')
+const methodOverride = require('method-override')
 
 const server = express()
 
-// server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
+server.use(methodOverride('_method'))
 server.use(routes)
 
 server.set('view engine', 'njk')
@@ -18,7 +20,7 @@ nunjucks.configure('views', {
 })
 
 server.get('/', (req, res) => {
-  return res.render('index', { recipes })
+  return res.render('index', { recipes: recipes.recipes })
 })
 
 server.get('/about', (req, res) => {
@@ -26,17 +28,17 @@ server.get('/about', (req, res) => {
 })
 
 server.get('/recipes', (req, res) => {
-  return res.render('recipes', { recipes })
+  return res.render('recipes', { recipes: recipes.recipes })
 })
 
 server.get("/recipes/:index", function (req, res) {
   const recipeIndex = req.params.index;
 
-  if (!recipes[recipeIndex]) {
+  if (!recipes.recipes[recipeIndex]) {
     return res.render('not-found')
   }
 
-  return res.render('recipe', { recipe: recipes[recipeIndex] })
+  return res.render('recipe', { recipe: recipes.recipes[recipeIndex] })
 })
 
 server.use((req, res) => {
