24faefe6cae8cb00669168ba26a893c570639420
Yuri Alves <yuripiresalves@gmail.com>
2020-11-12 18:17:16 -0300

    add chef model

---
+++ src/app/models/Chef.js
@@ -0,0 +1,75 @@
+const { date } = require('../../lib/utils')
+const db = require('../../config/db')
+
+module.exports = {
+  all(callback) {
+
+    db.query(`SELECT * FROM chefs`, (err, results) => {
+      if (err) throw `Database Error! ${err}`
+
+      callback(results.rows)
+    })
+
+  },
+  create(data, callback) {
+    const query = `
+      INSERT INTO chefs (
+        name,
+        avatar_url,
+        created_at
+      ) VALUES ($1, $2 ,$3)
+      RETURNING id
+    `
+
+    const values = [
+      data.name,
+      data.avatar_url,
+      date(Date.now()).iso
+    ]
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error! ${err}`
+
+      callback(results.rows[0])
+    })
+  },
+  find(id, callback) {
+    db.query(`
+      SELECT *
+      FROM chefs
+      WHERE id = $1`, [id], (err, results) => {
+        if (err) throw `Database Error! ${err}`
+
+        callback(results.rows[0])
+      })
+  },
+  update(data, callback) {
+    const query = `
+      UPDATE chefs SET
+        name=($1),
+        avatar_url=($2),
+        created_at=($3)
+      WHERE id = $4
+    ` 
+
+    const values = [
+      data.name,
+      data.avatar_url,
+      data.created_at,
+      data.id
+    ]
+
+    db.query(query, values, (err, results) => {
+      if (err) throw `Database Error! ${err}`
+
+      callback()
+    })
+  },
+  delete(id, callback) {
+    db.query(`DELETE FROM chefs WHERE id = $1`, [id], (err, results) => {
+      if (err) throw `Database Error! ${err}`
+
+      return callback()
+    })
+  }
+}
