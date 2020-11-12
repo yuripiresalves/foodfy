16dbc46f9f0998e8b4059b786a9cdd9bf6615278
Yuri Alves <yuripiresalves@gmail.com>
2020-11-12 18:15:31 -0300

    add chefs controller

---
+++ src/app/controllers/chefs.js
@@ -0,0 +1,52 @@
+const Chef = require('../models/Chef')
+
+module.exports = {
+  index(req, res) {
+
+    Chef.all((chefs) => {
+      return res.render('admin/chefs/index', { chefs})
+    })
+  },
+  create(req, res) {
+    return res.render('admin/chefs/create')
+  },
+  post(req, res) {
+
+    Chef.create(req.body, (chef) => {
+      return res.redirect(`admin/chefs/${chef.id}`)
+    })
+
+  },
+  show(req, res) {
+    
+    Chef.find(req.params.id, (chef) => {
+      if (!chef) return res.render('not-found')
+
+      return res.render('admin/chefs/show', { chef })
+    })
+
+  },
+  edit(req, res) {
+
+    Chef.find(req.params.id, (chef) => {
+      if (!chef) return res.render('not-found')
+
+      return res.render('admin/chefs/edit', { chef })
+    })
+
+
+  },
+  put(req, res) {
+
+    Chef.update(req.body, () => {
+      return res.redirect(`chefs/${req.body.id}`)
+    })
+
+  },
+  delete(req, res) {
+    
+    Chef.delete(req.body.id, () => {
+      return res.redirect('chefs')
+    })
+  }
+}
