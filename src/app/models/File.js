2d890cddf4827a64b3e4b9580bc33058a43580e6
Yuri Alves <yuripiresalves@gmail.com>
2020-12-22 15:36:01 -0300

    add createRecipeFile

--- src/app/models/File.js
+++ src/app/models/File.js
@@ -1,22 +1,77 @@
 const db = require('../../config/db')
+const fs = require('fs')
 
 module.exports = {
-  create({ filename, path, recipe_id }) {
-    const query = `
+  create({ filename, path }) {
+    try {
+      const query = `
       INSERT INTO files (
         name,
-        path,
-        recipe_id
-      ) VALUES ($1, $2 ,$3)
+        path
+      ) VALUES ($1, $2)
       RETURNING id
     `
 
-    const values = [
-      filename,
-      path,
-      recipe_id
-    ]
+      const values = [
+        filename,
+        path
+      ]
 
-    db.query(query, values)
+      return db.query(query, values)
+
+    } catch (err) {
+      console.log(err)
+    }
+
   },
+
+  async createRecipeFile({ filename, path, recipe_id }) {
+    try {
+      let query = `
+      INSERT INTO files (
+        name,
+        path
+      ) VALUES ($1, $2)
+      RETURNING id
+    `
+
+      let values = [
+        filename,
+        path
+      ]
+
+      const results = await db.query(query, values)
+      const fileId = results.rows[0].id
+
+      query = `
+          INSERT INTO recipe_files (
+            recipe_id,
+            file_id
+          ) VALUES ($1, $2)
+          RETURNING id
+        `
+
+      values = [
+        recipe_id,
+        fileId
+      ]
+
+      return db.query(query, values)
+
+    } catch (err) {
+      console.log(err)
+    }
+  },
+
+  async delete(id) {
+
+    const result = await db.query(`SELECT * FROM files WHERE id = $1`, [id])
+    const file = result.rows[0]
+
+    fs.unlinkSync(file.path)
+
+    return db.query(`
+      DELETE FROM files WHERE id = $1
+    `, [id])
+  }
 }
