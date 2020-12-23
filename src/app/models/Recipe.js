018623ce76362cb879128147f5f4a8267581b81e
Yuri Alves <yuripiresalves@gmail.com>
2020-12-23 16:39:41 -0300

    update functions

--- src/app/models/Recipe.js
+++ src/app/models/Recipe.js
@@ -1,36 +1,31 @@
 const { date } = require('../../lib/utils')
 const db = require('../../config/db')
+const fs = require('fs')
 
 module.exports = {
-  all(callback) {
+  all() {
 
-    db.query(`
+    return db.query(`
     SELECT recipes.*, chefs.name AS chef_name 
     FROM recipes
     LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-    ORDER BY recipes.id ASC`, (err, results) => {
-      if (err) throw `Database Error! ${err}`
-
-      callback(results.rows)
-    })
+    ORDER BY recipes.id ASC`)
 
   },
-  create(data, callback) {
+  create(data) {
     const query = `
       INSERT INTO recipes (
-        image,
         title,
         ingredients,
         preparation,
         information,
         created_at,
         chef_id
-      ) VALUES ($1, $2 ,$3, $4, $5, $6, $7)
+      ) VALUES ($1, $2 ,$3, $4, $5, $6)
       RETURNING id
     `
 
     const values = [
-      data.image,
       data.title,
       data.ingredients,
       data.preparation,
@@ -39,22 +34,14 @@ module.exports = {
       data.chef
     ]
 
-    db.query(query, values, (err, results) => {
-      if (err) throw `Database Error! ${err}`
-
-      callback(results.rows[0])
-    })
+    return db.query(query, values)
   },
   find(id, callback) {
-    db.query(`
+    return db.query(`
       SELECT recipes.*, chefs.name AS chef_name
       FROM recipes
       LEFT JOIN chefs ON (recipes.chef_id = chefs.id)
-      WHERE recipes.id = $1`, [id], (err, results) => {
-      if (err) throw `Database Error! ${err}`
-
-      callback(results.rows[0])
-    })
+      WHERE recipes.id = $1`, [id])
   },
   findBy(filter, callback) {
     db.query(`
@@ -68,20 +55,18 @@ module.exports = {
       callback(results.rows)
     })
   },
-  update(data, callback) {
+  update(data) {
     const query = `
       UPDATE recipes SET
-        image=($1),
-        title=($2),
-        ingredients=($3),
-        preparation=($4),
-        information=($5),
-        chef_id=($6)
-      WHERE id = $7
+        title=($1),
+        ingredients=($2),
+        preparation=($3),
+        information=($4),
+        chef_id=($5)
+      WHERE id = $6
     `
 
     const values = [
-      data.image,
       data.title,
       data.ingredients,
       data.preparation,
@@ -90,32 +75,36 @@ module.exports = {
       data.id
     ]
 
-    db.query(query, values, (err, results) => {
-      if (err) throw `Database Error! ${err}`
-
-      callback()
-    })
+    return db.query(query, values)
   },
-  delete(id, callback) {
-    db.query(`DELETE FROM recipes WHERE id = $1`, [id], (err, results) => {
-      if (err) throw `Database Error! ${err}`
+  async delete(id) {
 
-      return callback()
+    const results = await db.query(`
+      SELECT * FROM files
+      INNER JOIN recipe_files ON (files.id = recipe_files.file_id)
+      WHERE recipe_files.recipe_id = $1
+    `, [id])
+
+    const removedFiles = results.rows.map(file => {
+      fs.unlinkSync(file.path)
+
+      db.query(`DELETE FROM recipe_files WHERE recipe_files.file_id = $1`, [file.file_id])
+      db.query(`DELETE FROM files WHERE id = $1`, [file.file_id])
     })
+
+    return db.query(`DELETE FROM recipes WHERE id = $1`, [id])
   },
-  chefsSelectOptions(callback) {
-    db.query(`SELECT name, id FROM chefs`, (err, results) => {
-      if (err) throw `Database Error! ${err}`
+  chefsSelectOptions() {
 
-      callback(results.rows)
-    })
+    return db.query(`SELECT name, id FROM chefs`)
+    
   },
   paginate(params) {
     const { filter, limit, offset, callback } = params
 
     let query = "",
-        filterQuery = "",
-        totalQuery = `(
+      filterQuery = "",
+      totalQuery = `(
           SELECT count(*) from recipes
         ) AS total`
 
@@ -144,5 +133,12 @@ module.exports = {
 
       callback(results.rows)
     })
+  },
+  files(id) {
+    return db.query(`
+      SELECT files.* FROM files
+      LEFT JOIN recipe_files ON (files.id = recipe_files.file_id)
+      WHERE recipe_files.recipe_id = $1
+    `, [id])
   }
 }
