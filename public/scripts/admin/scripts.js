d3f5074d863d17593d75d348d7662d59e853dbd2
Yuri Alves <yuripiresalves@gmail.com>
2020-12-23 16:41:42 -0300

    add image gallery

--- public/scripts/admin/scripts.js
+++ public/scripts/admin/scripts.js
@@ -1,3 +1,16 @@
+const ImageGallery = {
+  highlight: document.querySelector('.gallery .highlight > img'),
+  previews: document.querySelectorAll('.gallery-preview img'),
+  setImage(e) {
+    const { target } = e
+
+    ImageGallery.previews.forEach(preview => preview.classList.remove('active'))
+    target.classList.add('active')
+
+    ImageGallery.highlight.src = target.src
+  }
+}
+
 // MENU ACTIVE
 const currentPage = location.pathname
 const menuItems = document.querySelectorAll('.menu-links a')
