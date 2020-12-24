528cf263eb2d378d6498387f0f17dc6f7526b836
Yuri Alves <yuripiresalves@gmail.com>
2020-12-24 15:24:40 -0300

    update chefs

--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const multer = require('./app/middlewares/multer')
 
 const site = require('./app/controllers/site')
 const recipes = require('./app/controllers/RecipeController')
-const chefs = require('./app/controllers/chefs')
+const chefs = require('./app/controllers/ChefController')
 
 // Site
 routes.get('/', site.index)
@@ -30,8 +30,8 @@ routes.get("/admin/chefs/create", chefs.create)
 routes.get("/admin/chefs/:id", chefs.show)
 routes.get("/admin/chefs/:id/edit", chefs.edit)
 
-routes.post("/admin/chefs", chefs.post)
-routes.put("/admin/chefs", chefs.put)
+routes.post("/admin/chefs", multer.array("photos", 1), chefs.post)
+routes.put("/admin/chefs", multer.array("photos", 1), chefs.put)
 routes.delete("/admin/chefs", chefs.delete)
 
 routes.use((req, res) => {  
