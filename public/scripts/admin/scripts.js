c9de21ad91ca011ee86335cc26ec4b59770171e4
Yuri Alves <yuripiresalves@gmail.com>
2021-01-01 17:28:40 -0300

    add email validate

--- public/scripts/admin/scripts.js
+++ public/scripts/admin/scripts.js
@@ -69,7 +69,7 @@ const PhotosUpload = {
     return false
   },
   getAllFiles() {
-    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer() 
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer()
 
     PhotosUpload.files.forEach(file => dataTransfer.items.add(file))
 
@@ -115,6 +115,48 @@ const PhotosUpload = {
     }
 
     photoDiv.remove()
+  }
+}
+
+const Validate = {
+  apply(input, func) {
+    Validate.clearErrors(input)
+
+    let results = Validate[func](input.value)
+    input.value = results.value
+
+    if (results.error) {
+      Validate.displayError(input, results.error)
+    }
+
+  },
+  displayError(input, error) {
+    const div = document.createElement('div')
+    div.classList.add('error')
+    div.innerHTML = error
+    input.parentNode.appendChild(div)
+    input.style.borderColor = 'red'
+    input.focus()
+  },
+  clearErrors(input) {
+    const errorDiv = input.parentNode.querySelector('.error')
+    if (errorDiv)
+      errorDiv.remove()
+      input.style.borderColor = '#DDD'
+  },
+  isEmail(value) {
+    let error = null
+
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+    if (!value.match(mailFormat))
+      error = "E-mail inválido!"
+
+
+    return {
+      error,
+      value
+    }
   }
 }
 
