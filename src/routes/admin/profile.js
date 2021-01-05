ae5189164403bbfd3db1093ac4048aab1af44a6e
Yuri Alves <yuripiresalves@gmail.com>
2021-01-05 12:25:33 -0300

    add onlyUsers

--- src/routes/admin/profile.js
+++ src/routes/admin/profile.js
@@ -4,8 +4,10 @@ const routes = express.Router()
 const ProfileController = require('../../app/controllers/ProfileController')
 const ProfileValidator = require('../../app/validators/profile')
 
+const { onlyUsers } = require('../../app/middlewares/session')
+
 // Rotas de perfil de um usuário logado
-routes.get('/profile', ProfileController.index) // Mostrar o formulário com dados do usuário logado
+routes.get('/profile', onlyUsers, ProfileController.index) // Mostrar o formulário com dados do usuário logado
 routes.put('/profile', ProfileValidator.update, ProfileController.put)// Editar o usuário logado
 
 module.exports = routes
