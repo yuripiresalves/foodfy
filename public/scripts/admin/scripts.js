9efd0b833161c951c276ec38403f9773064a269a
Yuri Alves <yuripiresalves@gmail.com>
2021-01-11 16:49:41 -0300

    update messages and add pagination

--- public/scripts/admin/scripts.js
+++ public/scripts/admin/scripts.js
@@ -47,7 +47,12 @@ const PhotosUpload = {
     const { files: fileList } = input
 
     if (fileList.length > uploadLimit) {
-      alert(`Envie no máximo ${uploadLimit} fotos!`)
+      const message = document.createElement('div')
+      message.classList.add('messages')
+      message.classList.add('error')
+      message.style.position = 'fixed'
+      message.innerHTML = `Envie no máximo ${uploadLimit} fotos!`
+      document.querySelector('body').append(message)
       event.preventDefault()
       return true
     }
@@ -61,7 +66,12 @@ const PhotosUpload = {
 
     const totalPhotos = fileList.length + photosDiv.length
     if (totalPhotos > uploadLimit) {
-      alert("Você atingiu o limite máximo de fotos!")
+      const message = document.createElement('div')
+      message.classList.add('messages')
+      message.classList.add('error')
+      message.style.position = 'fixed'
+      message.innerHTML = `Você atingiu o limite máximo de ${uploadLimit} fotos!`
+      document.querySelector('body').append(message)
       event.preventDefault()
       return true
     }
@@ -173,6 +183,72 @@ const Validate = {
         e.preventDefault()
       }
     }
+  }
+}
+
+// PAGINATION
+function paginate(totalPages, selectedPage) {
+  let pages = [],
+    oldPage
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+
+    const firstAndLastPages = currentPage == 1 || currentPage == 2 || currentPage == totalPages || currentPage == totalPages - 1
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 1
+    const pagesBeforeSelectedPage = currentPage >= selectedPage - 1
+
+    if (firstAndLastPages || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...")
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1)
+      }
+
+      pages.push(currentPage)
+
+      oldPage = currentPage
+    }
+  }
+  return pages
+}
+
+function createPagination(pagination) {
+  const filter = pagination.dataset.filter
+  const page = +pagination.dataset.page
+  const total = +pagination.dataset.total
+  const pages = paginate(total, page)
+
+  let elements = ""
+
+  for (let page of pages) {
+    if (String(page).includes("...")) {
+      elements += `<span>${page}</span>`
+    } else
+      if (filter) {
+        elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+      } else {
+        elements += `<a href="?page=${page}">${page}</a>`
+      }
+  }
+
+  pagination.innerHTML = elements
+}
+
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+  createPagination(pagination)
+}
+
+const elements = document.querySelectorAll('.pagination a')
+
+for (element of elements) {
+  if (location.search == "" || location.search.includes("?filter")) {
+    elements[0].classList.add('active')
+  } else if (location.search.includes(element.getAttribute('href'))) {
+    element.classList.add('active')
   }
 }
 
