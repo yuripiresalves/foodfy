a438fca423b3e3c6c648d7e765dd231d29d9952b
Yuri Alves <yuripiresalves@gmail.com>
2021-01-11 16:46:37 -0300

    add admin not-found page

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,13 +13,18 @@ routes.get('/recipes', SiteController.all)
 routes.get("/recipes/:id", SiteController.show)
 routes.get("/results", SiteController.results)
 routes.get("/chefs", SiteController.chefs)
+routes.get("/chefs/:id", SiteController.chef)
 
 // Recipes
 routes.use('/admin', adminRoutes)
 
 
-routes.use((req, res) => {  
-  return res.status(404).render('site/not-found')
+routes.use((req, res) => {
+  if (req.session.userId) {
+    return res.status(404).render('admin/parts/not-found')
+  } else {
+    return res.status(404).render('site/not-found')
+  }
 })
 
 module.exports = routes
