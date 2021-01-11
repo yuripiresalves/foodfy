06d4fc969c39431b4f90019599d948698ce31e03
Yuri Alves <yuripiresalves@gmail.com>
2021-01-11 16:58:54 -0300

    update session middleware

--- src/app/middlewares/session.js
+++ src/app/middlewares/session.js
@@ -1,25 +1,14 @@
 function onlyUsers(req, res, next) {
-  if (!req.session.userId)
+  if (!req.session.userId) {
     return res.redirect('/admin/login')
-
-  next()
-}
-
-function onlyUser(req, res, next) {
-  if (req.session.userId !== recipes.user_id)
-    return res.redirect('/admin/login')
+  }
 
   next()
 }
 
 function onlyAdmin(req, res, next) {
   if (!req.session.isAdmin) {
-    if (!req.session.userId) {
-      return res.redirect('/admin/login')
-    }
-    else {
-      return res.redirect('/admin/profile')
-    }
+    return res.redirect('/admin/profile')
   }
 
   next()
